cd96cf230c55ccdb77229b27f30df6f4efaa4b3d
Ivan Shimanski <ivan_shi@list.ru>
2021-06-14 14:23:25 +0300

    added file w task 1 (epam) w unit tests

---
+++ QA Automation Repository/HelperMethodesTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace MSTestsForTask1
+{
+    [TestClass]
+    public class HelperMethodesTests
+    {
+        [TestMethod]
+        [DataRow(5, "aaaaa", 0)]
+        [DataRow(4, "aaaaa", 1)]
+        [DataRow(0, "aaaaa", 1)]
+        [DataRow(0, "aA1@/*-&%$_?><[]+!~#;^()=", 25)]
+        [DataRow(0, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 52)]
+        [DataRow(0, "aaabcaaaa", 1)]
+        [DataRow(0, "abcabcaaaa", 3)]
+        [DataRow(0, null, 0)]
+        [DataRow(0, "", 0)]
+        public void FindLongestStringStartingFromAnElementNumberTest(int startSymbolNumber, string inputString, int result)
+        {
+            Assert.AreEqual(result, Helpers.FindLongestStringStartingFormAnElementNumber(startSymbolNumber, inputString));
+        }
+
+        [TestMethod]
+        [DataRow("aaaaaaaaa", 1)]
+        [DataRow("aaaaabaaa", 2)]
+        [DataRow("aA1@/*-&%$_?><[]+!~#;^()=", 25)]
+        [DataRow("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 52)]
+        [DataRow(null, 0)]
+        [DataRow("", 0)]
+
+        public void FindLongestStringTest(string inputString, int result)
+        {
+            Assert.AreEqual(result, Helpers.FindLongestString(new string[] { inputString }));
+        }
+    }
+}
---
+++ QA Automation Repository/Helpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class Helpers
+    {
+        public static int FindLongestString(string[] inputString) // methode finding the longest substring in which characters are not repeated
+        {
+            if (inputString.Length > 1  || inputString.Length == 0 || inputString[0] == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int maxLength = 0;
+                for (int j = 0; j < inputString[0].Length; j++)
+                {
+                    if (j + maxLength > inputString[0].Length)
+                    {
+                        break;
+                    }
+                    var tempLength = FindLongestStringStartingFormAnElementNumber(j, inputString[0]); // finding the longest substring starting at a given element number
+                    if (tempLength > maxLength)
+                    {
+                        maxLength = tempLength;
+                    }
+                }
+                return maxLength;
+            }
+        }
+
+        public static int FindLongestStringStartingFormAnElementNumber(int element_number, string inputString) // methode finding the longest substring starting at a given element number
+        {
+            if (inputString != null)
+            {
+                int maxLength = 0;
+                string symbolArray = "";
+                for (int i = element_number; i < inputString.Length; i++)
+                {
+                    if (i == element_number)
+                    {
+                        symbolArray += inputString[i];
+                        maxLength++;
+                    }
+                    else
+                    {
+                        if (!symbolArray.Contains(inputString[i]))
+                        {
+                            symbolArray += inputString[i];
+                            maxLength++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                return maxLength;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
