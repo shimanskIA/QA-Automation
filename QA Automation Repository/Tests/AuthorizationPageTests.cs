bed4115b1d81351ab44ced69f657993061a3f0a4
Ivan Shimanski <ivan_shi@list.ru>
2021-07-17 18:33:30 +0300

    added files w authorization tests

---
+++ QA Automation Repository/Tests/AuthorizationPageTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Task13.PageObjects;
+
+namespace Task13.Tests
+{
+    class AuthorizationPageTests
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl("https://gmail.com");
+        }
+
+        [Test]
+
+        public void SuccessfullLoginTest()
+        {
+            var authorizationPage = new AuthorizationPageObject(_webDriver);
+            authorizationPage
+                .Login(UserInputDataForTests.UserCorrectLogin, UserInputDataForTests.UserCorrectPassword)
+                .GoToAccountInfo();
+        }
+
+        [Test]
+
+        public void UnseccussfullLoginTest()
+        {
+            var authorizationPage = new AuthorizationPageObject(_webDriver);
+            Assert.AreEqual(authorizationPage.UnsuccessfullLogin(UserInputDataForTests.UserIncorrectLogin), "Не удалось найти аккаунт Google.");
+        }
+
+        [Test]
+        public void UnseccussfullPasswordTest()
+        {
+            var authorizationPage = new AuthorizationPageObject(_webDriver);
+            Assert.AreEqual(authorizationPage.UnsuccessfullPassword(UserInputDataForTests.UserCorrectLogin, UserInputDataForTests.UserIncorrectPassword), "Неверный пароль. Повторите попытку или нажмите на ссылку \"Забыли пароль?\", чтобы сбросить его.");
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+
+
+    }
+}
---
+++ QA Automation Repository/UserInputDataForTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Task13
+{
+    class UserInputDataForTests
+    {
+        public static string UserCorrectLogin { get; set; } = "epamfake1@gmail.com";
+
+        public static string UserIncorrectLogin { get; set; } = "epamfake";
+
+        public static string UserCorrectPassword { get; set; } = "fakepassword1";
+
+        public static string UserIncorrectPassword { get; set; } = "fakepassword";
+    }
+}
